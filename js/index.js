2c140170e944c96270ffd4610535841d0134f1f4
Andrii <fedorivandrij@gmail.com>
2020-03-14 17:17:48 +0000

    created js

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const display = document.querySelector('.display');
+
+document.querySelectorAll('.digits button')
+.forEach( button => button.addEventListener('click', digitsPressed));
+
+function digitsPressed(ev) {
+    display.value += ev.target.innerText;
+}
+
+document.querySelectorAll('.opers button')
+.forEach( button => button.addEventListener('click', opersPressed));
+
+function opersPressed(ev) {
+    display.value += ev.target.innerText;
+}
+
+document.querySelector('.equal').addEventListener('click', equalClicked);
+
+function equalClicked() {
+   display.value = eval(display.value);
+}
+
